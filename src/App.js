1ffe72dc61ed747cddd1c9d7b24abc656c718336
asherrow3 <asherrow3@gatech.edu>
2025-01-21 11:57:06 -0500

    prod

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 //data will be the string we send from our server
 const apiCall = () => {
-  axios.get('http://localhost:8080').then((data) => {
+  axios.get('https://qrbackend-13d260ac2b78.herokuapp.com/').then((data) => {
     //this console.log will be in our frontend console
     console.log(data)
   })
